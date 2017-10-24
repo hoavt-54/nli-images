a258332b1caeb48536a2342f24dbfb62dae6c813
Claudio Greco <claudio.greco@unitn.it>
2017-10-24 15:54:33 +0200

    Fixed script.

--- datasets/SICK/build_formatted_sick.py
+++ datasets/SICK/build_formatted_sick.py
@@ -5,7 +5,11 @@ if __name__ == "__main__":
     parser.add_argument("--filename", type=str, required=True)
     parser.add_argument("--converted_filename", type=str, required=True)
     args = args = parser.parse_args()
-    sick = pd.read_csv(args.filename, sep="\t", columns="")
+    sick = pd.read_csv(args.filename, sep="\t")
     converted_sick = sick[["entailment_label", "sentence_A", "sentence_B"]]
+    converted_sick.is_copy = False
+    converted_sick["entailment_label"] = converted_sick["entailment_label"].replace("ENTAILMENT", "entailment")
+    converted_sick["entailment_label"] = converted_sick["entailment_label"].replace("CONTRADICTION", "contradiction")
+    converted_sick["entailment_label"] = converted_sick["entailment_label"].replace("NEUTRAL", "neutral")
     converted_sick["jpg"] = "placeholder"
     converted_sick.to_csv(args.converted_filename, sep="\t", header=False, index=False)
