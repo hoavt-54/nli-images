242db65b7ecc3e9e89ae17447197c1d574b9352e
Claudio Greco <claudio.greco@gmx.com>
2017-11-11 15:56:25 +0100

    Update run_te_experiments.sh

--- models/run_te_experiments.sh
+++ models/run_te_experiments.sh
@@ -6,18 +6,18 @@
 
 ~/python3 train_te_baseline.py --train_filename=../datasets/snli_1.0/snli_1.0_train_filtered.tsv --dev_filename=../datasets/snli_1.0/snli_1.0_dev_filtered.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_save_filename=checkpoints/te/snli_train
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train --test_filename=../datasets/snli_1.0/snli_1.0_test_filtered.tsv --result_filename=checkpoints/te/snli_train_to_snli_test.txt
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train --test_filename=../datasets/snli_1.0/snli_1.0_test_filtered.tsv --result_filename=results/te/snli_train_to_snli_test.txt
 
 # Bowman et al. SNLI train -> SICK test
 
-~/python3 eval_te_baseline.py  --model_filename=checkpoints/te/snli_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=checkpoints/te/snli_train_to_SICK_test.tsv
+~/python3 eval_te_baseline.py  --model_filename=checkpoints/te/snli_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=results/te/snli_train_to_SICK_test.tsv
 
 # Bowman et al. SNLI train -> SICK2
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train --test_filename=../datasets/SICK/SICK2/SICK2.tsv --result_filename=checkpoints/te/snli_train_to_SICK2.tsv
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train --test_filename=../datasets/SICK/SICK2/SICK2.tsv --result_filename=results/te/snli_train_to_SICK2.tsv
 
 # Bowman et al. SICK train -> SICK test
 
 ~/python3 train_te_baseline.py --train_filename=../datasets/SICK/SICK/SICK_train.tsv --dev_filename=../datasets/SICK/SICK/SICK_dev.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_save_filename=checkpoints/te/SICK_train
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/SICK_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=checkpoints/te/SICK_train_to_SICK_test.tsv
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/SICK_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=results/te/SICK_train_to_SICK_test.tsv
