067f25d72fb5987180b3d72ebbfdb10284c677b5
Claudio Greco <claudio.greco@gmx.com>
2017-11-11 15:53:45 +0100

    Update run_te_experiments.sh

--- models/run_te_experiments.sh
+++ models/run_te_experiments.sh
@@ -5,44 +5,19 @@
 # Bowman et al. SNLI train -> SNLI test
 
 ~/python3 train_te_baseline.py --train_filename=../datasets/snli_1.0/snli_1.0_train_filtered.tsv --dev_filename=../datasets/snli_1.0/snli_1.0_dev_filtered.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_save_filename=checkpoints/te/snli_train
-echo -en '\n'
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train --test_filename=../datasets/snli_1.0/snli_1.0_test_filtered.tsv
-echo -en '\n'
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train --test_filename=../datasets/snli_1.0/snli_1.0_test_filtered.tsv --result_filename=checkpoints/te/snli_train_to_snli_test.txt
 
 # Bowman et al. SNLI train -> SICK test
 
-~/python3 eval_te_baseline.py  --model_filename=checkpoints/te/snli_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv
-echo -en '\n'
+~/python3 eval_te_baseline.py  --model_filename=checkpoints/te/snli_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=checkpoints/te/snli_train_to_SICK_test.tsv
 
 # Bowman et al. SNLI train -> SICK2
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train --test_filename=../datasets/SICK/SICK2/SICK2.tsv
-echo -en '\n'
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train --test_filename=../datasets/SICK/SICK2/SICK2.tsv --result_filename=checkpoints/te/snli_train_to_SICK2.tsv
 
 # Bowman et al. SICK train -> SICK test
 
 ~/python3 train_te_baseline.py --train_filename=../datasets/SICK/SICK/SICK_train.tsv --dev_filename=../datasets/SICK/SICK/SICK_dev.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_save_filename=checkpoints/te/SICK_train
-echo -en '\n'
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/SICK_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv
-echo -en '\n'
-
-## Results of the Recognizing Textual Inference task
-The results of the Recognizing Textual Inference task are reported in the following table:
-
-| train dataset | test dataset | Bowman et al. | BiMPM |
-|---------------|--------------|---------------|-------|
-| SNLI train    | SNLI test    |               |       |
-| SNLI train    | SICK test    |               |       |
-| SNLI train    | SICK2        |               |       |
-| SICK train    | SICK test    |               |       |
-
-## Results of the Recognizing Visual Textual Inference task
-The results of the Recognizing Visual Textual Inference task are reported in the following table:
-
-| train dataset | test dataset | Bowman et al. + images | V-BiMPM |
-|---------------|--------------|------------------------|---------|
-| VSNLI train   | VSNLI test   |                        |         |
-| VSNLI train   | VSICK        |                        |         |
-
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/SICK_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=checkpoints/te/SICK_train_to_SICK_test.tsv
