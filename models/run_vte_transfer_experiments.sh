f50a36a0ed0d6898890f72dbedb1307edeaebe75
Claudio Greco <claudio.greco@unitn.it>
2017-11-22 18:30:11 +0100

    Fixed sh scripts.

--- models/run_vte_transfer_experiments.sh
+++ models/run_vte_transfer_experiments.sh
@@ -4,6 +4,6 @@
 
 # Bowman et al. VSNLI train -> VSICK2 train -> VSICK2 test
 
-~/python3 train_vte_baseline.py --train_filename=../datasets/snli_1.0/snli_1.0_train_filtered.tsv --dev_filename=../datasets/snli_1.0/snli_1.0_dev_filtered.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --img_names_filename=../../flickr30k-cnn/flickr30k/filenames_77512.json --img_features_filename=../../flickr30k-cnn/flickr30k/vgg_feats_77512.npy --model_load_filename=checkpoints/vte/vsnli_train --model_save_filename=checkpoints/vte/vsnli_train_to_VSICK2_train
+~/python3 train_vte_baseline.py --train_filename=../datasets/SICK/VSICK2/VSICK2_train.tsv --dev_filename=../datasets/SICK/VSICK2/VSICK2_dev.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --img_names_filename=../../flickr30k-cnn/flickr30k/filenames_4096.json --img_features_filename=../../flickr30k-cnn/flickr30k/vgg_feats_4096.npy --model_load_filename=checkpoints/vte/vsnli_train --model_save_filename=checkpoints/vte/vsnli_train_to_VSICK2_train
 
-~/python3 eval_vte_baseline.py --model_filename=checkpoints/vte/vsnli_train --test_filename=../datasets/snli_1.0/snli_1.0_test_filtered.tsv --img_names_filename=../../flickr30k-cnn/flickr30k/filenames_77512.json --img_features_filename=../../flickr30k-cnn/flickr30k/vgg_feats_77512.npy --result_filename=results/vte/vsnli_train_to_VSICK2_train_to_VSICK2_test.txt
+~/python3 eval_vte_baseline.py --model_filename=checkpoints/vte/vsnli_train_to_VSICK2_train --test_filename=../datasets/SICK/VSICK2/VSICK2_test.tsv --img_names_filename=../../flickr30k-cnn/flickr30k/filenames_4096.json --img_features_filename=../../flickr30k-cnn/flickr30k/vgg_feats_4096.npy --result_filename=results/vte/vsnli_train_to_VSICK2_train_to_VSICK2_test.txt
