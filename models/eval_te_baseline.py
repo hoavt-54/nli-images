def72d057c65c5b5103f51bfcffe65b1b18b64bb
Claudio Greco <claudio.greco@unitn.it>
2017-11-24 16:33:40 +0100

    1) Added method to draw confusion matrix; 2) Added confusion matrix evaluation in the evaluation script for the TE baseline.

--- models/eval_te_baseline.py
+++ models/eval_te_baseline.py
@@ -1,4 +1,5 @@
 import atexit
+import csv
 import json
 import os
 import pickle
@@ -7,12 +8,13 @@ from argparse import ArgumentParser
 
 import numpy as np
 import tensorflow as tf
+from sklearn.metrics import confusion_matrix
 from tensorflow.core.protobuf import saver_pb2
 
 from dataset import load_te_dataset
 from logger import start_logger, stop_logger
 from train_te_baseline import build_te_baseline_model
-from utils import batch
+from utils import batch, draw_confusion_matrix
 
 if __name__ == "__main__":
     os.environ["PYTHONHASHSEED"] = "0"
@@ -25,7 +27,7 @@ if __name__ == "__main__":
     parser.add_argument("--model_filename", type=str, required=True)
     parser.add_argument("--result_filename", type=str, required=True)
     args = parser.parse_args()
-    start_logger(args.result_filename)
+    start_logger(args.result_filename + ".log")
     atexit.register(stop_logger)
 
     print("-- Loading params")
@@ -69,6 +71,8 @@ if __name__ == "__main__":
         test_num_examples = test_labels.shape[0]
         test_batches_indexes = np.arange(test_num_examples)
         test_num_correct = 0
+        y_true = []
+        y_pred = []
 
         for indexes in batch(test_batches_indexes, params["batch_size"]):
             test_batch_premises = test_premises[indexes]
@@ -83,5 +87,22 @@ if __name__ == "__main__":
                 }
             )
             test_num_correct += (predictions == test_batch_labels).sum()
+            with open(args.result_filename + ".predictions", mode="w") as out_file:
+                writer = csv.writer(out_file, delimiter="\t")
+                for i in range(len(indexes)):
+                    writer.writerow(
+                        [
+                            id2label[test_batch_labels[i]],
+                            id2label[predictions[i]],
+                            " ".join([id2token[id] for id in test_batch_premises[i] if id != token2id["#pad#"]]),
+                            " ".join([id2token[id] for id in test_batch_hypotheses[i] if id != token2id["#pad#"]])
+                        ]
+                    )
+                    y_true.append(id2label[test_batch_labels[i]])
+                    y_pred.append(id2label[predictions[i]])
         test_accuracy = test_num_correct / test_num_examples
         print("Mean test accuracy: {}".format(test_accuracy))
+        cm = confusion_matrix(y_true, y_pred)
+        classes = set(label2id.keys())
+        draw_confusion_matrix(cm, classes, args.result_filename + ".cm_unnormalized")
+        draw_confusion_matrix(cm, classes, args.result_filename + ".cm_normalized", normalized=True)
--- models/utils.py
+++ models/utils.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import tensorflow as tf
+import itertools
 
 
 def extract_axis_1(data, ind):
@@ -26,3 +29,29 @@ class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
         self.__dict__ = self
+
+
+def draw_confusion_matrix(cm, classes, filename, title="Confusion matrix", normalized=False, cmap=plt.cm.Blues):
+    if normalized:
+        cm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation="nearest", cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+    fmt = ".2f" if normalized else "d"
+    threshold = cm.max() / 2.0
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(
+            j,
+            i,
+            format(cm[i, j], fmt),
+            horizontalalignment="center",
+            color="white" if cm[i, j] > threshold else "black"
+        )
+    plt.tight_layout()
+    plt.ylabel("True label")
+    plt.xlabel("Predicted label")
+    plt.savefig(filename)
