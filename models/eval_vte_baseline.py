f135550964f9b2b0b8600e883b1a7bab33cbe20a
Claudio Greco <claudio.greco@unitn.it>
2017-12-04 21:06:43 +0100

    Improved VTE baseline evaluation script.

--- models/eval_vte_baseline.py
+++ models/eval_vte_baseline.py
@@ -1,4 +1,5 @@
 import atexit
+import csv
 import json
 import os
 import pickle
@@ -6,6 +7,7 @@ import random
 from argparse import ArgumentParser
 
 import numpy as np
+import pandas as pd
 import tensorflow as tf
 
 from dataset import ImageReader, load_vte_dataset
@@ -26,7 +28,7 @@ if __name__ == "__main__":
     parser.add_argument("--img_features_filename", type=str, required=True)
     parser.add_argument("--result_filename", type=str, required=True)
     args = parser.parse_args()
-    start_logger(args.result_filename)
+    start_logger(args.result_filename + ".log")
     atexit.register(stop_logger)
 
     print("-- Loading params")
@@ -44,8 +46,11 @@ if __name__ == "__main__":
         num_labels = len(label2id)
 
     print("-- Loading test set")
-    test_labels, test_premises, test_hypotheses, test_img_names = load_vte_dataset(args.test_filename, token2id,
-                                                                                   label2id)
+    test_labels, test_premises, test_hypotheses, test_img_names = load_vte_dataset(
+        args.test_filename,
+        token2id,
+        label2id
+    )
 
     print("-- Loading images")
     image_reader = ImageReader(args.img_names_filename, args.img_features_filename)
@@ -78,22 +83,42 @@ if __name__ == "__main__":
         test_num_examples = test_labels.shape[0]
         test_batches_indexes = np.arange(test_num_examples)
         test_num_correct = 0
+        y_true = []
+        y_pred = []
 
-        for indexes in batch(test_batches_indexes, params["batch_size"]):
-            test_batch_premises = test_premises[indexes]
-            test_batch_hypotheses = test_hypotheses[indexes]
-            test_batch_labels = test_labels[indexes]
-            batch_img_names = [test_img_names[i] for i in indexes]
-            batch_img_features = image_reader.get_features(batch_img_names)
-            predictions = session.run(
-                tf.argmax(logits, axis=1),
-                feed_dict={
-                    premise_input: test_batch_premises,
-                    hypothesis_input: test_batch_hypotheses,
-                    img_features_input: batch_img_features,
-                    dropout_input: 1.0
-                }
-            )
-            test_num_correct += (predictions == test_batch_labels).sum()
+        with open(args.result_filename + ".predictions", mode="w") as out_file:
+            writer = csv.writer(out_file, delimiter="\t")
+            for indexes in batch(test_batches_indexes, params["batch_size"]):
+                test_batch_premises = test_premises[indexes]
+                test_batch_hypotheses = test_hypotheses[indexes]
+                test_batch_labels = test_labels[indexes]
+                batch_img_names = [test_img_names[i] for i in indexes]
+                batch_img_features = image_reader.get_features(batch_img_names)
+                predictions = session.run(
+                    tf.argmax(logits, axis=1),
+                    feed_dict={
+                        premise_input: test_batch_premises,
+                        hypothesis_input: test_batch_hypotheses,
+                        img_features_input: batch_img_features,
+                        dropout_input: 1.0
+                    }
+                )
+                test_num_correct += (predictions == test_batch_labels).sum()
+                for i in range(len(indexes)):
+                    writer.writerow(
+                        [
+                            id2label[test_batch_labels[i]],
+                            id2label[predictions[i]],
+                            " ".join([id2token[id] for id in test_batch_premises[i] if id != token2id["#pad#"]]),
+                            " ".join([id2token[id] for id in test_batch_hypotheses[i] if id != token2id["#pad#"]]),
+                            batch_img_names[i]
+                        ]
+                    )
+                    y_true.append(id2label[test_batch_labels[i]])
+                    y_pred.append(id2label[predictions[i]])
         test_accuracy = test_num_correct / test_num_examples
         print("Mean test accuracy: {}".format(test_accuracy))
+        y_true = pd.Series(y_true, name="Actual")
+        y_pred = pd.Series(y_pred, name="Predicted")
+        confusion_matrix = pd.crosstab(y_true, y_pred, margins=True)
+        confusion_matrix.to_csv(args.result_filename + ".confusion_matrix")
