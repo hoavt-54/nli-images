f1c26b294233486d27627f90da48d46ff664044f
Claudio Greco <claudio.greco@unitn.it>
2017-12-05 11:50:58 +0100

    Fixed sh scripts.

--- models/run_te_experiments.sh
+++ models/run_te_experiments.sh
@@ -4,20 +4,20 @@
 
 # Bowman et al. SNLI train -> SNLI test
 
-~/python3 train_te_baseline.py --train_filename=../datasets/snli_1.0/snli_1.0_train_filtered.tsv --dev_filename=../datasets/snli_1.0/snli_1.0_dev_filtered.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_save_filename=checkpoints/te/snli_train
+~/python3 train_te_baseline.py --train_filename=../datasets/SNLI_1.0/SNLI_1.0_train_filtered.tsv --dev_filename=../datasets/SNLI_1.0/SNLI_1.0_dev_filtered.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_save_filename=checkpoints/te/SNLI_train
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train --test_filename=../datasets/snli_1.0/snli_1.0_test_filtered.tsv --result_filename=results/te/snli_train_to_snli_test.txt
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/SNLI_train --test_filename=../datasets/SNLI_1.0/SNLI_1.0_test_filtered.tsv --result_filename=results/te/SNLI_train_to_SNLI_test
 
 # Bowman et al. SNLI train -> SICK test
 
-~/python3 eval_te_baseline.py  --model_filename=checkpoints/te/snli_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=results/te/snli_train_to_SICK_test.tsv
+~/python3 eval_te_baseline.py  --model_filename=checkpoints/te/SNLI_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=results/te/SNLI_train_to_SICK_test
 
 # Bowman et al. SNLI train -> SICK2
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train --test_filename=../datasets/SICK/SICK2/SICK2.tsv --result_filename=results/te/snli_train_to_SICK2.tsv
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/SNLI_train --test_filename=../datasets/SICK/SICK2/SICK2.tsv --result_filename=results/te/SNLI_train_to_SICK2
 
 # Bowman et al. SICK train -> SICK test
 
 ~/python3 train_te_baseline.py --train_filename=../datasets/SICK/SICK/SICK_train.tsv --dev_filename=../datasets/SICK/SICK/SICK_dev.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_save_filename=checkpoints/te/SICK_train
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/SICK_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=results/te/SICK_train_to_SICK_test.tsv
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/SICK_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=results/te/SICK_train_to_SICK_test
--- models/run_te_transfer_experiments.sh
+++ models/run_te_transfer_experiments.sh
@@ -4,12 +4,12 @@
 
 # Bowman et al. SNLI train -> SICK train -> SICK test
 
-~/python3 train_te_baseline.py --train_filename=../datasets/SICK/SICK/SICK_train.tsv --dev_filename=../datasets/SICK/SICK/SICK_dev.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_load_filename=checkpoints/te/snli_train --model_save_filename=checkpoints/te/snli_train_to_SICK_train
+~/python3 train_te_baseline.py --train_filename=../datasets/SICK/SICK/SICK_train.tsv --dev_filename=../datasets/SICK/SICK/SICK_dev.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_load_filename=checkpoints/te/SNLI_train --model_save_filename=checkpoints/te/SNLI_train_to_SICK_train
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train_to_SICK_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=results/te/snli_train_to_SICK_train_to_SICK_test.txt
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/SNLI_train_to_SICK_train --test_filename=../datasets/SICK/SICK/SICK_test.tsv --result_filename=results/te/SNLI_train_to_SICK_train_to_SICK_test
 
 # Bowman et al. SNLI train -> SICK2 train -> SICK2 test
 
-~/python3 train_te_baseline.py --train_filename=../datasets/SICK/SICK2/SICK2_train.tsv --dev_filename=../datasets/SICK/SICK2/SICK2_dev.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_load_filename=checkpoints/te/snli_train --model_save_filename=checkpoints/te/snli_train_to_SICK2_train
+~/python3 train_te_baseline.py --train_filename=../datasets/SICK/SICK2/SICK2_train.tsv --dev_filename=../datasets/SICK/SICK2/SICK2_dev.tsv --vectors_filename=../../pre-wordvec/glove.840B.300d.txt --model_load_filename=checkpoints/te/SNLI_train --model_save_filename=checkpoints/te/SNLI_train_to_SICK2_train
 
-~/python3 eval_te_baseline.py --model_filename=checkpoints/te/snli_train_to_SICK2_train --test_filename=../datasets/SICK/SICK2/SICK2_test.tsv --result_filename=results/te/snli_train_to_SICK2_train_to_SICK2_test.txt
+~/python3 eval_te_baseline.py --model_filename=checkpoints/te/SNLI_train_to_SICK2_train --test_filename=../datasets/SICK/SICK2/SICK2_test.tsv --result_filename=results/te/SNLI_train_to_SICK2_train_to_SICK2_test
