8297242fe346a9003be56b556984e3755067ae30
Claudio Greco <claudio.greco@unitn.it>
2017-12-08 15:54:37 +0100

    Fixed script to run baselines on SICK difficult pairs.

--- models/run_sick_difficult_pairs_exp.sh
+++ models/run_sick_difficult_pairs_exp.sh
@@ -22,12 +22,12 @@
 
 # Recognizing Visual Textual Entailment without Transfer Learning
 
-~/python3 eval_bowman_vte_baseline.py --model_filename=checkpoints/bowman_VTE/VSNLI_train --test_filename=../datasets/SICK/SICK2/difficult_SICK2.tsv --img_names_filename=../../flickr8k-cnn/flickr8k/filenames_4096.json --img_features_filename=../../flickr8k-cnn/flickr8k/vgg_feats_4096.npy --result_filename=results/bowman_VTE/VSNLI_train_to_difficult_SICK2
+~/python3 eval_bowman_vte_baseline.py --model_filename=checkpoints/bowman_VTE/VSNLI_train --test_filename=../datasets/SICK/VSICK2/difficult_VSICK2.tsv --img_names_filename=../../flickr8k-cnn/flickr8k/filenames_4096.json --img_features_filename=../../flickr8k-cnn/flickr8k/vgg_feats_4096.npy --result_filename=results/bowman_VTE/VSNLI_train_to_difficult_VSICK2
 
-~/python3 eval_lstm_vte_baseline.py --model_filename=checkpoints/lstm_VTE/VSNLI_train --test_filename=../datasets/SICK/SICK2/difficult_SICK2.tsv --img_names_filename=../../flickr8k-cnn/flickr8k/filenames_4096.json --img_features_filename=../../flickr8k-cnn/flickr8k/vgg_feats_4096.npy --result_filename=results/lstm_VTE/VSNLI_train_to_difficult_SICK2
+~/python3 eval_lstm_vte_baseline.py --model_filename=checkpoints/lstm_VTE/VSNLI_train --test_filename=../datasets/SICK/VSICK2/difficult_VSICK2.tsv --img_names_filename=../../flickr8k-cnn/flickr8k/filenames_4096.json --img_features_filename=../../flickr8k-cnn/flickr8k/vgg_feats_4096.npy --result_filename=results/lstm_VTE/VSNLI_train_to_difficult_VSICK2
 
 # Recognizing Visual Textual Entailment with Transfer Learning
 
-~/python3 eval_bowman_vte_baseline.py --model_filename=checkpoints/bowman_VTE/VSNLI_train_to_VSICK2_train --test_filename=../datasets/SICK/SICK2/difficult_SICK2.tsv --img_names_filename=../../flickr8k-cnn/flickr8k/filenames_4096.json --img_features_filename=../../flickr8k-cnn/flickr8k/vgg_feats_4096.npy --result_filename=results/bowman_VTE/VSNLI_train_to_VSICK2_train_to_difficult_SICK2
+~/python3 eval_bowman_vte_baseline.py --model_filename=checkpoints/bowman_VTE/VSNLI_train_to_VSICK2_train --test_filename=../datasets/SICK/VSICK2/difficult_VSICK2.tsv --img_names_filename=../../flickr8k-cnn/flickr8k/filenames_4096.json --img_features_filename=../../flickr8k-cnn/flickr8k/vgg_feats_4096.npy --result_filename=results/bowman_VTE/VSNLI_train_to_VSICK2_train_to_difficult_VSICK2
 
-~/python3 eval_lstm_vte_baseline.py --model_filename=checkpoints/lstm_VTE/VSNLI_train_to_VSICK2_train --test_filename=../datasets/SICK/SICK2/difficult_SICK2.tsv --img_names_filename=../../flickr8k-cnn/flickr8k/filenames_4096.json --img_features_filename=../../flickr8k-cnn/flickr8k/vgg_feats_4096.npy --result_filename=results/lstm_VTE/VSNLI_train_to_VSICK2_train_to_difficult_SICK2
+~/python3 eval_lstm_vte_baseline.py --model_filename=checkpoints/lstm_VTE/VSNLI_train_to_VSICK2_train --test_filename=../datasets/SICK/VSICK2/difficult_VSICK2.tsv --img_names_filename=../../flickr8k-cnn/flickr8k/filenames_4096.json --img_features_filename=../../flickr8k-cnn/flickr8k/vgg_feats_4096.npy --result_filename=results/lstm_VTE/VSNLI_train_to_VSICK2_train_to_difficult_VSICK2
