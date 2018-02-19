b51bdac2b88f5613d8957caa29f85bc3eed03573
Claudio Greco <claudio.greco@unitn.it>
2018-02-19 11:49:43 +0100

    Fixed script.

--- models/split_ic_dataset.py
+++ models/split_ic_dataset.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -12,9 +13,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     dataset = pd.read_csv(args.dataset_filename, sep="\t")
-    dataset_X = dataset.ix[:, 1:]
-    dataset_y = dataset.ix[:, 0]
-    training_set, test_set = train_test_split(
+    dataset_X = dataset.ix[:, 1:].values
+    dataset_y = dataset.ix[:, 0].values
+
+    X_train, X_test, y_train, y_test = train_test_split(
         dataset_X,
         dataset_y,
         test_size=0.2,
@@ -22,16 +24,26 @@ if __name__ == "__main__":
         random_state=12345
     )
 
-    training_set_X = training_set.ix[:, 1:]
-    training_set_y = training_set.ix[:, 0]
-    training_set, validation_set = train_test_split(
-        training_set_X,
-        training_set_y,
+    X_train, X_dev, y_train, y_dev = train_test_split(
+        X_train,
+        y_train,
         test_size=0.2,
-        stratify=training_set_y,
+        stratify=y_train,
         random_state=12345
     )
 
-    training_set.to_csv(args.training_set_filename, sep="\t", index=False)
-    test_set.to_csv(args.test_set_filename, sep="\t", index=False)
-    validation_set.to_csv(args.validation_set_filename, sep="\t", index=False)
+    y_train = y_train.reshape(y_train.shape[0], 1)
+    y_test = y_test.reshape(y_test.shape[0], 1)
+    y_dev = y_dev.reshape(y_dev.shape[0], 1)
+
+    training_set = np.concatenate((y_train, X_train), axis=1)
+    test_set = np.concatenate((y_test, X_test), axis=1)
+    validation_set = np.concatenate((y_dev, X_dev), axis=1)
+
+    training_set = pd.DataFrame(training_set)
+    test_set = pd.DataFrame(test_set)
+    validation_set = pd.DataFrame(validation_set)
+
+    training_set.to_csv(args.training_set_filename, sep="\t", index=False, header=False)
+    test_set.to_csv(args.test_set_filename, sep="\t", index=False, header=False)
+    validation_set.to_csv(args.validation_set_filename, sep="\t", index=False, header=False)
