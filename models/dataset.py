5b88e3e1f5a46982c3ab6afaa446be546b02b14e
Claudio Greco <claudio.greco@unitn.it>
2018-02-21 00:52:21 +0100

    Fixed minor things.

--- models/dataset.py
+++ models/dataset.py
@@ -20,10 +20,7 @@ def load_te_dataset(filename, token2id, label2id):
     with open(filename) as in_file:
         reader = csv.reader(in_file, delimiter="\t")
 
-        i = 0
         for row in reader:
-            print(i)
-            i += 1
             labels.append(label2id[row[0].strip()])
             premise_tokens = row[1].strip().split()
             hypothesis_tokens = row[2].strip().split()
