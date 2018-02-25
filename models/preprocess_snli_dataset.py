beaf39e12fa76c9c99ef6994a39abf8cfbc2454f
Claudio Greco <claudio.greco@unitn.it>
2018-02-25 18:50:55 +0100

    Improved script.

--- models/preprocess_snli_dataset.py
+++ models/preprocess_snli_dataset.py
@@ -12,11 +12,18 @@ def extract_tokens_from_binary_parse(parse):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--dataset_filename", type=str, required=True)
+    parser.add_argument("--hoa_dataset_filename", type=str, required=True)
     parser.add_argument("--preprocessed_dataset_filename", type=str, required=True)
     args = parser.parse_args()
 
     num_lines = len([1 for line in open(args.dataset_filename)])
     progress = Progbar(num_lines)
+
+    available_images = set()
+    with open(args.hoa_dataset_filename) as in_file:
+        reader = csv.reader(in_file, delimiter="\t")
+        for row in reader:
+            available_images.add(row[3].strip())
 
     with open(args.dataset_filename) as in_file:
         with open(args.preprocessed_dataset_filename, mode="w") as out_file:
@@ -31,9 +38,9 @@ if __name__ == "__main__":
                 hypothesis_tokens = extract_tokens_from_binary_parse(data["sentence2_binary_parse"])
                 label = data["gold_label"]
                 image_filename = data["captionID"].split("#")[0]
+                if image_filename in available_images and label != "-":
+                    writer.writerow([label, " ".join(premise_tokens), " ".join(hypothesis_tokens), image_filename, premise, hypothesis])
                 progress.update(row_number)
-
-                writer.writerow([label, " ".join(premise_tokens), " ".join(hypothesis_tokens), image_filename, premise, hypothesis])
             else:
                 print("Invalid dataset format!")
                 exit(-1)
